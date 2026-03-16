d4e8bde442e3089ac3761f9af3866a0db2a24389
FedeSasHub <fsavio2003@gmail.com>
2026-03-16 14:29:58 +0100

    Initial commit

---
+++ Source/PAA_Videogame2026/PAA_Videogame2026.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "PAA_Videogame2026.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, PAA_Videogame2026, "PAA_Videogame2026" );
